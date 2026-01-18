4f87d462954fd79d7f4ded8244c7ba5c4289c025
Dash <95099676+peter-kiilu@users.noreply.github.com>
2026-01-18 20:27:50 +0300

    splash

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      includeAssets: ["vite.png"],
       manifest: {
         name: "MeStock - Inventory Management",
         short_name: "MeStock",
@@ -22,17 +22,17 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "vite.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "vite.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "vite.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
